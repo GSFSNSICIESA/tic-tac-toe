41b3ec899d48f81873c348c3acfbbc54938863c0
gsfsnsiciesa <2083886+GSFSNSICIESA@users.noreply.github.com>
2023-11-01 16:13:36 +0200

    first steps and some pseudocode

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+this is tic tac toe game: 
+one time we allow the user to put X
+the other time we allow the user to put O 
+repeat until one wins
+winning when numbers are matched 
+
+
+
+
+
+
+
+
+
+*/
